d066aac65180142ac5930453ca69ecc9c2a3eb27
Sebastián Pinto-Agüero González <sebastian.pintoaguero@usach.cl>
2018-04-26 15:12:11 -0300

    DH en Python ordenado

--- DH.py
+++ DH.py
@@ -1,27 +1,7 @@
 import math
 import numpy as np
 
-PI = math.pi
-
-x = np.array([1,5,2])
-y = np.array([7,4,1])
-
-suma = x + y
-
-A = np.matrix( 
-	( 	
-		(2,3),
-		(3,5) 
-		) 
-	)
-B = np.matrix( ( (1,2),(5,-1) ) )
-
-#############
-
 def matrizAi(teta, d, a, alfa):
-
-	#math.radians(Grados) = radianes
-
 	A = np.matrix(
 		(
 			( math.cos(teta), -math.cos(alfa)*math.sin(teta), math.sin(alfa)*math.sin(teta)	, a*math.cos(teta) ),
@@ -29,17 +9,44 @@ def matrizAi(teta, d, a, alfa):
 			( 0				, math.sin(alfa)				, math.cos(alfa)				, d ),
 			( 0				, 0								, 0								, 1 )
 			)
-
 		)
 	return A
 
-A1 = matrizAi(math.radians(0), 10, 0, math.radians(90))
-A2 = matrizAi(math.radians(0), 0, 10, math.radians(0))
-A3 = matrizAi(math.radians(0), 0, 10, math.radians(0))
+def calcularT(a, b, c):
+	return a*b*c
 
-T = A1*A2*A3
+def obtenerX(t):
+	return t[0,3]
 
-X = T[0,3]
-Y = T[1,3]
-Z = T[2,3]
+def obtenerY(t):
+	return t[1,3]
 
+def obtenerZ(t):
+	return t[2,3]
+
+#math.radians(Grados) = radianes
+
+# Parametros obtenidos del DH
+
+#	i 		tetai		di		ai 		alfai		
+#
+#	1		q1			L1		0		90
+#
+#	2		q2			0		L2		0
+#
+#	3		Q3			0		L3		0
+
+
+# L1 = 6 cm
+# L2 = 8 cm
+# L3 = 12,5c m
+
+A1 = matrizAi(math.radians(0), 6, 0, math.radians(90))
+A2 = matrizAi(math.radians(0), 0, 8, math.radians(0))
+A3 = matrizAi(math.radians(0), 0, 12.5, math.radians(0))
+
+T = calcularT(A1,A2,A3)
+
+X = obtenerX(T)
+Y = obtenerY(T)
+Z = obtenerZ(T)
